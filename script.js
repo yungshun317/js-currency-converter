d4906af121fcd9c8819fc0f06babd42b04c3400d
yungshun317 <seraph317@gmail.com>
2023-08-23 04:36:40 +0800

    [FEAT] Use the Fetch API to retrieve data

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const inputAmount = document.querySelector('.currency-converter__input-oamount');
-const fromCurrency = document.querySelector('.currency-converter__input-from');
-const toCurrency = document.querySelector('.currency-converter__input-to');
+const inputFromCurrency = document.querySelector('.currency-converter__input-from');
+const inputToCurrency = document.querySelector('.currency-converter__input-to');
+const btnConvert = document.querySelector('.currency-converter__btn--convert');
+const btnReset = document.querySelector('.currency-converter__btn--reset');
 let fromValue, toValue, inputValue;
 
-fromCurrency.addEventListener('change', function(e) {
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
+
+inputFromCurrency.addEventListener('change', function(e) {
     fromValue = `${e.target.value}`;
 });
 
-toCurrency.addEventListener('change', function(e) {
+inputToCurrency.addEventListener('change', function(e) {
     toValue = `${e.target.value}`;
 });
 
 inputAmount.addEventListener('input', function(e) {
     inputValue = `${e.target.value}`;
+});
+
+btnConvert.addEventListener('click', function(e) {
+    fetch(`${api}`)
+        .then(currency => {
+            console.log(currency);
+            return currency.json();
+        })
+        .then();
 });
