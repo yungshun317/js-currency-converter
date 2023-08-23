321f382defd0a5e8cd70c3b2aeb9aebde1ea2b10
yungshun317 <seraph317@gmail.com>
2023-08-24 04:48:50 +0800

    [FEAT] Display results after conversion

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputFromCurrency = document.querySelector('.currency-converter__input-fro
 const inputToCurrency = document.querySelector('.currency-converter__input-to');
 const btnConvert = document.querySelector('.currency-converter__btn--convert');
 const btnReset = document.querySelector('.currency-converter__btn--reset');
+const finalAmount = document.querySelector('.currency-converter__final-amount');
 let fromValue, toValue, inputValue;
 
 const api = "https://api.exchangerate-api.com/v4/latest/USD";
@@ -20,10 +21,20 @@ inputAmount.addEventListener('input', function(e) {
 });
 
 btnConvert.addEventListener('click', function(e) {
+    console.log(fromValue);
+    console.log(toValue);
+    console.log(inputValue);
     fetch(`${api}`)
         .then(currency => {
             console.log(currency);
             return currency.json();
         })
-        .then();
+        .then(displayResults);
 });
+
+function displayResults(currency) {
+    let fromRate = currency.rates[inputFromCurrency];
+    let toRate = currency.rates[inputToCurrency];
+    finalAmount.innerHTML = ((toRate / fromRate) * inputValue).toFixed(2);
+    finalAmount.style.display = 'block';
+}
